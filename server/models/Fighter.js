411c36197cc955d5a22f8949f66c5e4e780cb958
DrummD <drummd@rocketmail.com>
2022-12-08 10:09:35 -0800

    updated/1

--- server/models/Fighter.js
+++ server/models/Fighter.js
@@ -2,7 +2,30 @@
 const { Schema } = require('mongoose');
 
 const fighterSchema = new Schema({
-    
+    name: {
+        type: String,
+        required: true
+    },
+    strength: {
+        type: Number,
+        required: true
+    },
+    defense: {
+        type: Number,
+        required: true
+    },
+    agility: {
+        type: Number,
+        required: true
+    },
+    will: {
+        type: Number,
+        required: true
+    },
+    chance: {
+        type: Number,
+        required: true
+    }
 })
 
 module.exports = fighterSchema;
--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,3 @@
 const User = require('./User');
 const Universe = require('./Universe')
-module.exports = { User };
+module.exports = { User, Universe };
